84ce7a8b714b036b6cc8fa4d53b28041e8836ef5
JHSONG <jhsong4754@gmail.com>
2021-02-03 15:14:22 +0900

    Final

--- calculator.py
+++ calculator.py
@@ -4,14 +4,10 @@ def add(a, b):
     return (a+b)
 
 def subtract(a, b):
-<<<<<<< HEAD
-  return (a-b)
-=======
     return (a-b)
 
 def multiply(a, b):
     return (a*b)
->>>>>>> main
 
 def divide(a, b):
     return (a/b)
